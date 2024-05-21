c96bac024a1fc5daa0b6e981244abaf3c8789155
zzha142 <zzha142@outlook.com>
2024-05-21 14:01:21 -0600

    update style for pages

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
 export default function Home() {
-  const linkStyles = "underline text-cyan-600 hover:text-cyan-300";
+  const linkStyles = "underline hover:text-cyan-600 font-bold text-xl";
 
   return (
-   <main className="h-screen">
-    <h1 className="text-xl">CPRG 306: Web Development2 - Assignments</h1>
-    <ul>
-      <li><Link className={linkStyles} href="./week-2/">Week 2 Assignments</Link></li>
-    </ul>
-   </main>
+    <main className="h-screen items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">CPRG 306: Web Development2 - Assignments</h1>
+      <ul>
+        <li><Link className={linkStyles} href="./week-2/">Week 2 Assignments</Link></li>
+      </ul>
+    </main>
   );
 }
