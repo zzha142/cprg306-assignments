4278dc4e45d27cac989e8a91700ec7aed1d4a21a
zzha142 <zzha142@outlook.com>
2024-05-24 13:21:56 -0600

    w3-assignment
    update for color

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 
 export default function Page(){
     return(
-        <main className="f-screen">
+        <main className="f-screen bg-white">
             <h1 className="font-bold text-4xl">Shopping List</h1>
             <ItemList />
         </main>
