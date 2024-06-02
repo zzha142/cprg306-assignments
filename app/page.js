faf826a30e883dbd639e58c9bf2688336378e455
zzha142 <zzha142@outlook.com>
2024-06-02 17:44:57 -0600

    week4
    added week4 assignment page

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
       <ul>
         <li><Link className={linkStyles} href="./week-2/">Week 2 Assignment</Link></li>
         <li><Link className={linkStyles} href="./week-3/">Week 3 Assignment</Link></li>
+        <li><Link className={linkStyles} href="./week-4/">Week 4 Assignment</Link></li>
       </ul>
     </main>
   );
---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+
+export default function Page(){
+    return(
+        <main className="h-screen">
+            <NewItem />
+        </main>
+    )
+}
