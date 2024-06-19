e60d9cb91c18462a6a31b60e6a6f724fa3edbeec
zzha142 <zzha142@outlook.com>
2024-06-18 20:09:12 -0600

    Update item-list.js

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -15,26 +15,52 @@ export default function ItemList(){
     )
 
     let [sort, setSort] = useState("name");
-
-    // if (sort !="name") {
-    //     itemArray = itemArray.sort((item)=>item.category === sort);
-    // }
-
+    
+    //sorted item by name or category: depending on which button is clicked & the default setting is name
+    if (sort === "name") {
+        itemArray.sort((a,b)=>a.name.localeCompare(b.name));
+    }
+    else if (sort ==="category"){
+        itemArray.sort((a,b)=>a.category.localeCompare(b.category));
+    }
+    
     const handleSortChange = (event) =>setSort(event.target.value);
 
     return (
         <section>
             <div className="flex w-1/2">
                 <label className="font-bold text-xl">Sort By:</label>
-                <button className={buttonStyle} value="name" onClick={handleSortChange}>Name</button>
-                <button className={buttonStyle} value="category" onClick={handleSortChange}>Category</button>
-                <button className={buttonStyle} >Grouped Category</button>
+                <button
+                    className={`${buttonStyle} ${sort === "name" ? activeStyle : inactiveStyle}`}
+                    value="name"
+                    onClick={handleSortChange}
+                >
+                    Name
+                </button>
+                <button
+                    className={`${buttonStyle} ${sort === "category" ? activeStyle : inactiveStyle}`}
+                    value="category"
+                    onClick={handleSortChange}
+                >
+                    Category
+                </button>
+                <button>Grouped Category</button>
             </div>
 
             <div>
                 {itemArray.map((item)=>(
                     <Item itemObj = {item}/>
                 ))}
+                
+                {/* {itemArray.map((item)=> {
+                    if(newcategory){
+                        (<h2></h2>
+                            <ul></ul>
+                        )
+                    }
+                }
+                
+                )} */}
             </div>
         </section>
     )
