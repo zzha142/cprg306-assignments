e11c7c12845f51ecf6dceb247c3bfdd0922a3526
zzha142 <zzha142@outlook.com>
2024-06-23 02:13:24 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ export default function Home() {
         <li><Link className={linkStyles} href="./week-3/">Week 3 Assignment</Link></li>
         <li><Link className={linkStyles} href="./week-4/">Week 4 Assignment</Link></li>
         <li><Link className={linkStyles} href="./week-5/">Week 5 Assignment</Link></li>
+        <li><Link className={linkStyles} href="./week-6/">Week 6 Assignment</Link></li>
 
       </ul>
     </main>
